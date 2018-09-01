ecaf5b6934fc7a61702ee09f5bbcc382554ff820
Eduardo de Lima Ramos <ramos.eduardo87@gmail.com>
2018-09-01 02:47:59 -0300

    First version

---
+++ src/BookList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Book from './Book';
+
+const BookList = (props) => {
+  return (
+    <div>
+      {props.bookList.map((book) => {
+        return (
+          <Book key={book.id} book={book.volumeInfo} className="Book"
+          />
+        )
+      })
+      }
+    </div>
+  );
+}
+
+export default BookList;
